4a18cf57df672ef84f2ecc7c636ba4afe1aedf52
RiFrost <ri.frost@web.de>
2021-04-22 17:36:55 +0200

    changed gradle.build settings

--- src/main/java/com/webtech/infocard/HomeController.java
+++ src/main/java/com/webtech/infocard/HomeController.java
@@ -1,13 +1,14 @@
 package com.webtech.infocard;
 
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @CrossOrigin
 public class HomeController {
   
-  @GetMapping("/")
+  @GetMapping("/api")
   public String home() {
     return "InfoCard - Your new Way of learning!";
   }
