b5da486ad2c60f157c6810a09a3d1a63f05a8326
RiFrost <ri.frost@web.de>
2021-04-22 17:11:43 +0200

    init

---
+++ src/test/java/com/webtech/infocard/InfocardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.webtech.infocard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InfocardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
