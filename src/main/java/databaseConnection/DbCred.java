a2f03fb3d8c82d09cb85758e6466a39c934a2db3
Richard Frost <45118822+RiFrost@users.noreply.github.com>
2021-04-26 16:13:36 +0200

    prepared DbCred for local dev

---
+++ src/main/java/databaseConnection/DbCred.java
@@ -0,0 +1,11 @@
+package databaseConnection;
+
+public interface DbCred {
+
+        final String driverClass = "driver_class";
+        final String url = "db_url";
+        final String user = "user";
+        final String password = "password";
+        final String schema = "scheme";
+
+}
