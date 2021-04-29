04d576d668fc59ace4328b00c0607ce20f9074f9
MariaHa247 <maria.hallmann@posteo.de>
2021-04-29 18:26:21 +0200

    moved InfoApplication into webtech package

---
+++ src/main/java/com/webtech/InfocardApplication.java
@@ -0,0 +1,21 @@
+package com.webtech;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+
+
+@SpringBootApplication
+public class InfocardApplication{
+	
+	public static void main(String[] args) {
+		SpringApplication.run(InfocardApplication.class, args);
+	}
+
+}
+
+
+		
+		
+
