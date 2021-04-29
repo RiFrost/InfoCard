ba88ee187174fbf69c81b5d35ba2b954d59cec23
MariaHa247 <maria.hallmann@posteo.de>
2021-04-29 17:59:36 +0200

    added new db connection with Spring Data JPA

--- src/main/java/com/webtech/infocard/ApiController.java
+++ src/main/java/com/webtech/infocard/ApiController.java
@@ -1,26 +1,45 @@
 package com.webtech.infocard;
 
+import com.webtech.databaseConnection.DbConnection;
+import com.webtech.databaseConnection.Topic;
+import com.webtech.databaseConnection.TopicRepository;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+
+
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import databaseConnection.DbConnection;
+
 
 @RestController
 @CrossOrigin
 @RequestMapping("/api")
 public class ApiController {
+
+  @Autowired
+	private TopicRepository topicRepository;
   
   @GetMapping("/test")
   public String home() {
     return "InfoCard - Your new Way of learning!";
   }
 
-  @GetMapping("/db")
-  public String db() {
-    boolean checkDbConnection = DbConnection.getInstance().checkDbConnection();
-    return checkDbConnection ? "DB Verbindung erfolgreich" : "DB Verbindung fehlgeschlagen";
+  // @GetMapping("/db")
+  // public String db() {
+  //   boolean checkDbConnection = DbConnection.getInstance().checkDbConnection();
+  //   return checkDbConnection ? "DB Verbindung erfolgreich" : "DB Verbindung fehlgeschlagen";
+  // }
+
+  @GetMapping("/topicList")
+  public List<Topic> getTopicList(){
+    List<Topic> topicList = topicRepository.findAll();
+    return topicList;
   }
 
 }
