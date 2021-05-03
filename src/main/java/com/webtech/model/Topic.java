8ce8cb076d9408da6e4c2181452a4db99220d2e4
MariaHa247 <maria.hallmann@posteo.de>
2021-05-03 20:24:22 +0200

    reimplemented Topic Entity

--- src/main/java/com/webtech/model/Topic.java
+++ src/main/java/com/webtech/model/Topic.java
@@ -1,47 +1,70 @@
 package com.webtech.model;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 import javax.persistence.*;
+import java.util.HashSet;
 
 @Entity
 @Table(name = "topic")
 public class Topic {
 
     @Id
-    @Column(name = "topic_id")
+    @Column(name = "topic_id", nullable = false)
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @JsonIgnore
+    @OneToMany(mappedBy = "topic", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+    private Set<IndexCard> indexCards = new HashSet<>();
 
-    private Long user_id;
+    @ManyToOne()
+    @JoinColumn(name = "customer_id", nullable = false)
+    private User user;
 
-    @Column(name = "des")
-    private String topicname;
 
+    @Column(name = "des", nullable = false, length = 100)
+    private String topicName;
+
+    public Topic() {
+    }
+
+    public Topic(User user, String topicName) {
+        this.user = user;
+        this.topicName = topicName;
+    }
+
+    
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public Long getId() {
+        return id;
+    }
 
     public void setId(Long id) {
         this.id = id;
     }
 
-    public Long getUser_id() {
-        return user_id;
+    public String getTopicName() {
+        return topicName;
     }
 
-    public void setUser_id(Long user_id) {
-        this.user_id = user_id;
+    public void setTopicName(String topicName) {
+        this.topicName = topicName;
     }
 
-
-    public String getTopicname() {
-        return topicname;
-    }
-
-    public void setTopicname(String topicname) {
-        this.topicname = topicname;
+    public Set<IndexCard> getIndexCards() {
+        return indexCards;
     }
 
     @Override
     public String toString() {
-        return "Topic [id = " + id + ", userid = " + user_id + ", topic description = " + topicname + "]";
+        return "TopicId=" + id + ", topicName='" + topicName;
     }
-
 }
