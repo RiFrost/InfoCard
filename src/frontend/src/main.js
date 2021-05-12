5169127aa2f613630a6e046641ad89a8516361c4
RiFrost <ri.frost@web.de>
2021-05-12 12:17:45 +0200

    added element-plus as css framework

--- src/frontend/src/main.js
+++ src/frontend/src/main.js
@@ -2,8 +2,11 @@ import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
+import "element-plus/lib/theme-chalk/index.css";
+import ElementPlus from "element-plus";
 
 createApp(App)
 	.use(store)
 	.use(router)
+	.use(ElementPlus)
 	.mount("#app");
