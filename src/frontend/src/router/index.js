65ed920db554c353113e2c68b39c44b5c7875629
RiFrost <ri.frost@web.de>
2021-05-23 14:42:31 +0200

    += frontend check for valid user; += redirect to login page if not valid

--- src/frontend/src/router/index.js
+++ src/frontend/src/router/index.js
@@ -1,35 +1,47 @@
 import { createRouter, createWebHistory } from "vue-router";
+import store from "../store";
 import Home from "@/views/Home.vue";
 import Login from "@/views/Login.vue";
 import Register from "@/views/Register.vue";
 import Topics from "@/views/Topics.vue";
 
 const routes = [
-	{
-		path: "/",
-		name: "Home",
-		component: Home
-	},
-	{
-		path: "/login",
-		name: "Login",
-		component: Login
-	},
-	{
-		path: "/register",
-		name: "Register",
-		component: Register
-	},
-	{
-		path: "/topics",
-		name: "Topics",
-		component: Topics
-	}
+  {
+    path: "/",
+    name: "Home",
+    component: Home
+  },
+  {
+    path: "/login",
+    name: "Login",
+    component: Login
+  },
+  {
+    path: "/register",
+    name: "Register",
+    component: Register
+  },
+  {
+    path: "/topics",
+    name: "Topics",
+    component: Topics,
+    meta: {
+      requieresLogin: true
+    }
+  }
 ];
 
 const router = createRouter({
-	history: createWebHistory(),
-	routes
+  history: createWebHistory(),
+  routes
+});
+
+router.beforeEach(async (to, from, next) => {
+  const hasUser = store.getters["user/isAuthenticated"];
+  const requieresLogin = to.matched.some(record => record.meta.requieresLogin);
+
+  if (!hasUser && requieresLogin) next({ name: "Login" });
+  else next();
 });
 
 export default router;
