03fa72b64a35388ec43c3a789bb8bcd7853594bc
MariaHa247 <maria.hallmann@posteo.de>
2021-06-27 19:12:04 +0200

    implemented favorite function for cards in backend

--- src/main/java/com/webtech/database/repository/IndexCardRepository.java
+++ src/main/java/com/webtech/database/repository/IndexCardRepository.java
@@ -11,8 +11,18 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface IndexCardRepository extends JpaRepository<IndexCard, Long> {
 
-  @Query(value ="SELECT * FROM Indexcard i INNER JOIN Topic t on i.topic_id = t.topic_id WHERE t.topic_id = :topic_id ORDER BY i.card_id ASC", nativeQuery = true)
-  List<IndexCard> findAllIndexCardsByTopicId(@Param("topic_id")Long topicId);
+  // @Query(value ="SELECT * FROM Indexcard i INNER JOIN Topic t on i.topic_id = t.topic_id WHERE t.topic_id = :topic_id ORDER BY i.card_id ASC", nativeQuery = true)
+  // List<IndexCard> findAllCardsByTopicId(@Param("topic_id")Long topicId);
+
+  @Query(value ="SELECT card_id, i.topic_id, question, answer, is_favored FROM Indexcard i " +
+  "INNER JOIN Topic t on i.topic_id = t.topic_id inner join users u on u.users_id = t.users_id " +
+  "WHERE t.topic_id = :topic_id ORDER BY i.card_id ASC", nativeQuery = true)
+  List<IndexCard> findAllCardsByTopicId(@Param("topic_id")Long topicId);
+
+  @Query(value ="SELECT card_id, i.topic_id, question, answer, is_favored " +
+  "FROM Indexcard i INNER JOIN Topic t on i.topic_id = t.topic_id inner join users u on u.users_id = t.users_id " +
+  "WHERE is_favored = true and u.users_id = :users_id ORDER BY i.card_id ASC", nativeQuery = true)
+  List<IndexCard> findFavoriteCards(@Param("users_id") String userId);
 
   void deleteById(Long id);
 }
